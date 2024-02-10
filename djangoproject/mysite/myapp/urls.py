cda8aa5f7422132e9149cfbcf8e699c59a4174af
Philipp Medoev <phil.medoev@gmail.com>
2024-02-10 13:59:43 +0300

    hw 3 is done

--- djangoproject/mysite/myapp/urls.py
+++ djangoproject/mysite/myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, about
+from .views import home, about, ClientOrdersView, ClientProductsView
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('client_orders/', ClientOrdersView.as_view(), name='client_orders'),
+    path('client_products/', ClientProductsView.as_view(), name='client_products')
 ]
--- djangoproject/mysite/myapp/views.py
+++ djangoproject/mysite/myapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Client, Product, Order
+from datetime import datetime, timedelta
 import logging
+
+
+class ClientOrdersView(View):
+    def get(self, request):
+        clients = Client.objects.all()
+        return render(request, 'client_orders.html', {'clients': clients})
+
+
+class ClientProductsView(View):
+    def get(self, request):
+        client = Client.objects.first()
+        today = datetime.now()
+        period = request.GET.get('period', '7')  # default period - week
+        start_date = today - timedelta(days=int(period))
+        orders = Order.objects.filter(client=client, order_date__gte=start_date, order_date__lte=today)
+        products = Product.objects.filter(order__in=orders).distinct()
+        return render(request, 'client_products.html', {'client': client, 'products': products})
+
 
 logger = logging.getLogger(__name__)
 
@@ -28,4 +49,3 @@ def about(request):
     logger.info("Пользователь посетил страницу 'О себе'")
 
     return render(request, 'myapp/about.html', {'html_content': html_content})
-
