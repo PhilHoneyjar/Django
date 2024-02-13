b8fcb80a5dfa3a22c6e26b73b503093486726956
Philipp Medoev <phil.medoev@gmail.com>
2024-02-13 20:44:52 +0300

    hw3 refactored. added commands, added db to vc, added templates

--- djangoproject/e_store/e_store_app/forms.py
+++ djangoproject/e_store/e_store_app/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Client
+from .models import Client, Product
 
 
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'email', 'phone_number', 'address']
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'quantity', 'date_added']
--- djangoproject/e_store/e_store_app/models.py
+++ djangoproject/e_store/e_store_app/models.py
@@ -18,14 +18,14 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
-    added_date = models.DateTimeField(default=timezone.now)
+    date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'Product {self.name}, price: {self.price}, quantity: {self.quantity}'
 
 
 class Order(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='orders')
     products = models.ManyToManyField(Product)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     order_date = models.DateTimeField(default=timezone.now)
