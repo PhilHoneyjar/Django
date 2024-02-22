3a7d48c0d14205b9dde869804715fea7e3276696
Philipp Medoev <phil.medoev@gmail.com>
2024-02-22 20:00:09 +0300

    Registered Models

--- djangoproject/e_store/e_store_app/admin.py
+++ djangoproject/e_store/e_store_app/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from .models import Client, Order, Product
 
-# Register your models here.
 
-admin.site.register(Client)
-admin.site.register(Product)
-admin.site.register(Order)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'registration_date')
+    search_fields = ('name', 'email', 'phone')
 
-# ...
+
+admin.site.register(Client, ClientAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'date_added')
+    list_filter = ('date_added',)
+    search_fields = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('client', 'total_amount', 'order_date')
+    list_filter = ('order_date',)
+    search_fields = ('client__name', 'products__name')
+
+
+admin.site.register(Order, OrderAdmin)
